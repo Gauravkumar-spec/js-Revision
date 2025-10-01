49621413041a4160b1c295aaf19483169dc4fee3
Kumar Gaurav <k6gaurav66@gmail.com>
2025-10-01 23:17:53 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("Hello") // we r using nodeJs, not browser
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // an object
+
